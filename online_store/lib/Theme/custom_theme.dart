97284e48431fb5d59fc930915e94ee814c967c76
nirajan1 <nirajanjoshi80@gmail.com>
2023-12-30 08:17:09 +0545

    custom app icon added

--- online_store/lib/Theme/custom_theme.dart
+++ online_store/lib/Theme/custom_theme.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 final ThemeData myTheme = ThemeData(
   useMaterial3: true,
-  colorScheme: const ColorScheme.light(
-    primary: Color(0XFFCF7650),
-    background: Colors.white,
-    inversePrimary: Colors.white,
+  // colorScheme: const ColorScheme.light(
+  //   primary: Color(0XFFCF7650),
+  //   background: Colors.white,
+  //   inversePrimary: Colors.white,
+  // ),
+  primaryColor: const Color(0XFFCF7650),
+  textTheme: const TextTheme(
+    headlineMedium: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+    ),
   ),
 );
--- online_store/lib/View/home_view.dart
+++ online_store/lib/View/home_view.dart
@@ -5,6 +5,16 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Wellcome to home page,'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
